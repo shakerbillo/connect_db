15ced38769496bb975debec22264f8ca18f21c38
emmanuelgyan-amoah <xgyanamo@mendelu.cz>
2024-06-25 06:22:45 +0200

    first commit

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+	const [availablePlaces, setAvailablePlaces] = useState([]);
+
+	useEffect(() => {
+		const fetchPlaces = async () => {
+			const res = await fetch('http://localhost:3000/places');
+			const data = await res.json();
+			setAvailablePlaces(data.places);
+		};
+    fetchPlaces();
+	}, []);
+	return (
+		<Places
+			title='Available Places'
+			places={availablePlaces}
+			fallbackText='No places available.'
+			onSelectPlace={onSelectPlace}
+		/>
+	);
 }
